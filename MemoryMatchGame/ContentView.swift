f24b9cad8d859f0814a5b7650ba6c643037b3fae
Manish-bol <manish.kumar@lji.io>
2025-01-31 11:16:07 +0530

    Initial Commit

---
+++ MemoryMatchGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryMatchGame
+//
+//  Created by Manish Kumar on 31/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryMatchGame/MemoryMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchGameApp.swift
+//  MemoryMatchGame
+//
+//  Created by Manish Kumar on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
